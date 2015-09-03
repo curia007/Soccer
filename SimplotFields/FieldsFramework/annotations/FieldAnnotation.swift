d8082443551e2f5ef12eb0d8daa5f54ef7f51bcf
Carmelo Uria <curia@carmelouria.com>
2015-09-03 16:53:23 -0600

    Moved FieldAnnotation to Static Framework

---
+++ SimplotFields/FieldsFramework/annotations/FieldAnnotation.swift
@@ -0,0 +1,31 @@
+//
+//  FieldAnnotation.swift
+//  SimplotFields
+//
+//  Created by Carmelo I. Uria on 9/3/15.
+//  Copyright © 2015 Carmelo I. Uria. All rights reserved.
+//
+
+import UIKit
+
+import MapKit
+
+public class FieldAnnotation: NSObject, MKAnnotation
+{
+    public var title: String?
+    public var subtitle: String?
+    
+    public var fieldName: String
+
+    public var coordinate: CLLocationCoordinate2D
+
+    public init(_ coordinate: CLLocationCoordinate2D, title: String, subtitle: String, fieldName: String)
+    {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle + ":  " + fieldName
+        self.fieldName = fieldName
+    }
+    
+
+}

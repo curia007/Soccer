a261bbda5537ee6d0f5d56cdc1521a4c5f75b49a
Carmelo Uria <curia@carmelouria.com>
2015-09-20 13:47:20 -0600

    Removed Parse comments and cleaned up the images

--- SimplotFields/SimplotFields/AppDelegate.swift
+++ SimplotFields/SimplotFields/AppDelegate.swift
@@ -21,9 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.setupUserNotificationActions()
         
-        // start locating babysitter
-        //Parse.setApplicationId("xjCTHk0lLTrDyCVzBQZDElyivece4dUYLqYpeJNt", clientKey: "4LjnzcomCgvvSagerROGPbop1dC9JSlQLeC04vYo")
-        
         return true
     }
 
@@ -62,9 +59,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     {
         debugPrint("\(__FUNCTION__):  deviceToken: \(deviceToken)")
         
-        //let installation = PFInstallation.currentInstallation()
-        //installation.setDeviceTokenFromData(deviceToken)
-        //installation.saveInBackground()
     }
     
     func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError)
